5c3d37289f1e44e99afb96978df872b4f3012a93
shufito <jshufito@gmail.com>
2023-04-23 11:03:16 -0300

    Animation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
+import Image from "next/image";
 import axios from "axios";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaWater, FaWind } from "react-icons/fa";
 import { MdLocationOn, MdLocationOff } from "react-icons/md";
-import InfoClima from "../components/InfoClima.jsx";
 
 export default function Home() {
   const API_KEY = "f0367477e5f44cfe05e56fd865d2bed3";
   const [search, setSearch] = useState("");
   const [info, setInfo] = useState([]);
   const [erro, setErro] = useState(null);
+  const [height, setHeight] = useState({});
+  const [fadeIn, setFadeIn] = useState({ opacity: 0, scale: 0 });
 
   const handleClickSearch = async () => {
     axios
@@ -17,20 +19,28 @@ export default function Home() {
       )
       .then((resposta) => {
         console.log(resposta.data);
+        setFadeIn({ opacity: 0, scale: 0 });
         setInfo(resposta.data);
         setErro(null);
+        setHeight({ height: 380 });
+        setFadeIn({ opacity: 100, scale: 1 });
       })
       .catch((erro) => {
         console.log("Ocorreu um erro: ", erro);
+        setFadeIn({ opacity: 0, scale: 0 });
         setInfo(false);
         setErro(true);
+        setHeight({ height: 200 });
       });
   };
 
   return (
     <>
       <div className="flex items-center justify-center h-screen bg-[#0F131F] ">
-        <div className="relative bg-slate-300 px-8 py-6 overflow-hidden rounded-2xl m-4 lg:m-0">
+        <div
+          className="flex flex-col justify-center relative bg-slate-300 px-8 py-6 overflow-hidden rounded-2xl mx-4 lg:mx-0 h-16 transition-all duration-500"
+          style={height}
+        >
           <div className="flex items-center justify-center w-full h-min">
             <MdLocationOn className="text-[#0F131F] text-3xl font-bold" />
             <input
@@ -59,7 +69,44 @@ export default function Home() {
             </p>
           </div>
 
-          {info.main && <InfoClima data={info} />}
+          {info.main && (
+            <div>
+              <div className="text-center mt-6 transition-all duration-500 delay-1000"
+              style={fadeIn}>
+                <div className="flex items-center justify-center mt-8 ">
+                  <Image
+                    src={`./img/${info.weather[0]?.icon}.png`}
+                    alt="/"
+                    width="100"
+                    height="100"
+                  />
+                </div>
+                <p className="relativo text-[#0F131F] text-6xl font-bold -ml-2">
+                  {parseInt(info.main?.temp - 273.15)}
+                  <span className="absolute text-2xl ml-2">°C</span>
+                </p>
+                <p className="text-[#0F131F] text-2xl font-medium capitalize">
+                  {info.weather[0]?.description}
+                </p>
+              </div>
+              <div className="flex items-center justify-around mt-8">
+                <div className="flex items-center gap-3 justify-start">
+                  <FaWater className="text-[#0F131F] font-medium text-2xl" />
+                  <div className="">
+                    <span>{info.main?.humidity}%</span>
+                    <p>Umidade</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-3 justify-end">
+                  <FaWind className="text-[#0F131F] font-medium text-2xl" />
+                  <div className="">
+                    <span>{info.wind?.speed} m/s</span>
+                    <p>Vento</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
